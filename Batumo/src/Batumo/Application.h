f853f2da1a30e92686eb5cf22e28ad51ff2468d4
Murilo Sobreiro <murilosobreiro@hotmail.com>
2023-10-11 13:16:17 -0300

    Add: Hookup dispatcher for GLFW events

--- Batumo/src/Batumo/Application.h
+++ Batumo/src/Batumo/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Batumo/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Batumo {
@@ -13,7 +15,10 @@ namespace Batumo {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Runnig = true;
 	};
