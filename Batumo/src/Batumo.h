4583c0ebace54dffe9bd1063c4a59b8a81ff7e79
Murilo Sobreiro <murilosobreiro@hotmail.com>
2023-10-18 11:53:07 -0300

    Add: Key and Mouse codes

--- Batumo/src/Batumo.h
+++ Batumo/src/Batumo.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "Batumo/Application.h"
-#include "Batumo/Log.h"
 #include "Batumo/Layer.h"
+#include "Batumo/Log.h"
+
+#include "Batumo/Input.h"
+#include "Batumo/Events/KeyCodes.h"
+#include "Batumo/Events/MouseButtonCodes.h"
 
 #include "Batumo/ImGui/ImGuiLayer.h"
 
